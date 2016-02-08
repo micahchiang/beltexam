eef43eb1c63a4ffc8cc87dc255c44656e905c95f
micah.chiang <micahkchiang@gmail.com>
2016-02-08 11:06:15 -0800

    fixed config

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/client'));
 
-require('./server/config/mongoose.js');
-require('./server/config/routes.js')(app);
+require('./server/Config/mongoose.js');
+require('./server/Config/routes.js')(app);
 
 app.listen(8888, function(){
 	console.log("Node Online at port 8888");
